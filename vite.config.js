24feec7d56f1f92a1e56769d33b1d72c20bcf402
Lutfi Azis <17lutfi21@gmail.com>
2022-12-20 09:08:40 +0700

    feat: Reusable header

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "partials"),
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        "play/selection": resolve(__dirname, "play", "play/selection.html"),
+        "play/selection": resolve(__dirname, "play", "selection.html"),
       },
     },
   },
